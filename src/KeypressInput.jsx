557692192e07c4d0cb8688610b48ffe34ff42044
Conner Charlebois <cdcharlebois@gmail.com>
2020-11-03 17:37:04 -0500

    add interval

--- src/KeypressInput.jsx
+++ src/KeypressInput.jsx
@@ -1,4 +1,4 @@
-import { Component, createElement } from "react";
+import { Component, createElement, useRef } from "react";
 import { TextInput, View, Text } from "react-native";
 import { flattenStyles } from "./utils/common";
 const defaultStyle = {
@@ -7,12 +7,19 @@ const defaultStyle = {
     input: {}
 };
 export const KeypressInput = props => {
+    const pendingRun = useRef(false);
     const styles = flattenStyles(defaultStyle, props.style);
     const { attr, action } = props;
     const handleChangeText = text => {
         attr.setValue(text);
-        if (action && action.canExecute) {
-            action.execute();
+        if (pendingRun.current === false) {
+            pendingRun.current = true;
+            setTimeout(() => {
+                pendingRun.current = false;
+                if (action && action.canExecute) {
+                    action.execute();
+                }
+            }, props.interval);
         }
     };
     return <TextInput style={styles.input} value={attr} onChangeText={handleChangeText} />;
